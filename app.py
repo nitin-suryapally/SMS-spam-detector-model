c0d29be52ee3ebf32f5e99df019be469e4c1bedc
Nitin suryapally <suryapallyn@gmail.com>
2023-03-20 23:44:40 +0530

    end

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+
+import pickle
+import string
+import nltk
+# import sklearn
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+# function for preprocessing
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("SMS Spam Detector")
+
+input_sms = st.text_input("Enter the Message")
+
+if st.button('predict'):
+
+    # 1.preprocess
+    transformed_text = transform_text(input_sms)
+    # 2.vectorize
+    vector_input = tfidf.transform([transformed_text])
+    # 3.pridict
+    result = model.predict(vector_input)[0]
+    # 4.display
+
+    if result == 1:
+        st.header("Spam")
+
+    else:
+        st.header("Not Spam")
